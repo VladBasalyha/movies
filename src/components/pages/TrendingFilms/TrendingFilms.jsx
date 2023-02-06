3ac147097b0a22ae7d8da8a424e474b63dbf845a
Vladyslav Basalyha <100045506+VladBasalyha@users.noreply.github.com>
2023-02-07 00:52:28 +0200

    updates

--- src/components/pages/TrendingFilms/TrendingFilms.jsx
+++ src/components/pages/TrendingFilms/TrendingFilms.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { ListOfFilms } from 'components/CommonComponents/ListOfFilms/ListOfFilms';
-import { fetchMoviesById, fetchTrendingMovies } from 'API/api';
+import { fetchTrendingMovies } from 'API/api';
 export const TrendingFilms = () => {
   const [trendingFilms, setTrendingFilms] = useState([]);
   useEffect(() => {
