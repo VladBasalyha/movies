e5c76c72663351ee4dddba0c244fb411c36fa5a8
Vladyslav Basalyha <100045506+VladBasalyha@users.noreply.github.com>
2023-02-14 14:28:18 +0200

    correc

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -21,8 +21,8 @@ export const App = () => {
             <Route path="cast" element={<Cast />} />
             <Route path="reviews" element={<ReviewComponent />} />
           </Route>
-          <Route path="*" element={<Navigate to="/" />} />
         </Route>
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </div>
   );
